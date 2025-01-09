OCA/purchase-workflow

[FIX] purchase_order_type: sequence depending on date ranges
Custom sequence for an order type wasn't take in account date ranges
during order creation, this fix it.

purchase_order_type/models/purchase_order.py
@@ -48,7 +48,9 @@ class PurchaseOrder(models.Model):
         if vals.get("name", "/") == "/" and vals.get("order_type"):
             purchase_type = self.env["purchase.order.type"].browse(vals["order_type"])
             if purchase_type.sequence_id:
-                vals["name"] = purchase_type.sequence_id.next_by_id()
+                vals["name"] = purchase_type.sequence_id.next_by_id(
+                    sequence_date=vals.get("date_order")
+                )
         return super().create(vals)
 
     @api.constrains("company_id")

adhoc-cicd/oca-purchase-workflow

purchase_order_type/models/purchase_order.py
@@ -42,7 +42,9 @@ class PurchaseOrder(models.Model):
                     values["order_type"]
                 )
                 if purchase_type.sequence_id:
-                    values["name"] = purchase_type.sequence_id.next_by_id()
+                    values["name"] = purchase_type.sequence_id.next_by_id(
+                        sequence_date=values.get("date_order")
+                    )
         return super().create(vals_list)
 
     @api.constrains("company_id")