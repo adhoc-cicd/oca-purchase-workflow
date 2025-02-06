3eec094455b61c25657e1c1558f7fa87d268c8ce
Rafael Lima <rafael.lima@camptocamp.com>
2025-02-06 20:12:44 +0100

    [MIG] purchase_invoice_plan: Migration to 18.0

--- purchase_invoice_plan/tests/test_purchase_invoice_plan.py
+++ purchase_invoice_plan/tests/test_purchase_invoice_plan.py
@@ -5,7 +5,7 @@ from freezegun import freeze_time
 
 from odoo import fields
 from odoo.exceptions import UserError, ValidationError
-from odoo.tests.common import Form, TransactionCase
+from odoo.tests import Form, TransactionCase
 
 
 class TestPurchaseInvoicePlan(TransactionCase):
