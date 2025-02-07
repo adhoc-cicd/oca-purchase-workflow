d0fd46a70f99fbd0e0d29088dab9ed21d025d8f6
Rafael Lima <rafael.lima@camptocamp.com>
2025-02-07 17:00:40 +0100

    [IMP] purchase_invoice_plan: use BaseCommon in tests

--- purchase_invoice_plan/tests/test_purchase_invoice_plan.py
+++ purchase_invoice_plan/tests/test_purchase_invoice_plan.py
@@ -5,10 +5,12 @@ from freezegun import freeze_time
 
 from odoo import fields
 from odoo.exceptions import UserError, ValidationError
-from odoo.tests import Form, TransactionCase
+from odoo.tests import Form
+
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestPurchaseInvoicePlan(TransactionCase):
+class TestPurchaseInvoicePlan(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
